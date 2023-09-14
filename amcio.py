76bddc92916a7f00ab05e3ab834d4e401de98257
Alessio Mazzella <alessio.gs2@gmail.com>
2023-09-14 14:46:45 +0200

    make refresh function and move code to buildui function

--- amcio.py
+++ amcio.py
@@ -22,4 +22,4 @@ def readModlists(htmls):
                     value = modlists[i][j].split('"', 2)[1]
                     if len(value.split("=")) > 1:   mods.update({key: value.split("=")[-1]})
                     else:   dlcs.update({key: value.rsplit("/", 1)[1]})
-    return mods, dlcs
+    return modlists, mods, dlcs
